b47a5260900a95b096ae363a6444446a7f32ab10
Van Pho Nguyen <vanpho02@gmail.com>
2017-10-13 20:03:52 +0700

    xong

---
+++ dist/a.js
@@ -0,0 +1,7 @@
+"use strict";
+var age = 100;
+function sayHello(name) {
+    return 'Xin chao ' + name;
+}
+var sayHello2 = function (name) { return 'Xin chao ' + name; };
+console.log(sayHello2('Pho'));
---
+++ src/a.ts
@@ -0,0 +1,9 @@
+let age: number = 100;
+
+function sayHello(name: string): string {
+    return 'Xin chao ' + name;
+}
+
+const sayHello2 = (name: string): string => 'Xin chao ' + name;
+
+console.log(sayHello2('Pho'));
